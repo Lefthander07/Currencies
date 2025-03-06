ff691336d666aaf1e9c9f927da7dada4e34580a4
Alekseev Andrey <alekseev.anrdey@nanocad.ru>
2025-03-06 20:39:29 +0300

    money implemented

--- Homework1/Domain/Money.cs
+++ Homework1/Domain/Money.cs
@@ -1,4 +1,7 @@
-﻿namespace Fuse8.BackendInternship.Domain;
+﻿using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace Fuse8.BackendInternship.Domain;
 
 /// <summary>
 /// Модель для хранения денег
@@ -12,6 +15,9 @@ public class Money
 
 	public Money(bool isNegative, int rubles, int kopeks)
 	{
+		//Валидация входных параметров
+		if (rubles < 0 || kopeks < 0 || kopeks > 99)
+			throw new ArgumentException("Рубли не могут быть отрицательными, и копейки должны быть в диапазоне [0, 9]");
 		IsNegative = isNegative;
 		Rubles = rubles;
 		Kopeks = kopeks;
@@ -31,4 +37,81 @@ public class Money
 	/// Количество копеек
 	/// </summary>
 	public int Kopeks { get; }
+
+    public static Money operator +(Money lhs, Money rhs)
+    {
+		int rubles = lhs.Rubles + rhs.Rubles;
+		int kopeks = lhs.Kopeks + rhs.Kopeks;
+
+        if (kopeks >= 100)
+        {
+            kopeks -= 100;
+            rubles += 1;
+        }
+
+        return new Money(rubles, kopeks);
+    }
+
+    public static Money operator -(Money lhs, Money rhs)
+    {
+        int rubles = lhs.Rubles - rhs.Rubles;
+        int kopeks = lhs.Kopeks - rhs.Kopeks;
+
+        if (kopeks < 0)
+        {
+            kopeks += 100;
+            rubles -= 1;
+        }
+
+        return new Money(rubles, kopeks);
+    }
+
+    public static bool operator >(Money lhs, Money rhs)
+    {
+		return lhs.Rubles * 100 + lhs.Kopeks > rhs.Rubles * 100 + rhs.Kopeks;
+    }
+
+    public static bool operator >=(Money lhs, Money rhs)
+    {
+        return lhs.Rubles * 100 + lhs.Kopeks >= rhs.Rubles * 100 + rhs.Kopeks;
+    }
+
+    public static bool operator <(Money lhs, Money rhs)
+    {
+        return rhs > lhs;
+    }
+
+    public static bool operator <=(Money lhs, Money rhs)
+    {
+        return rhs >= lhs;
+    }
+
+    public static bool operator ==(Money lhs, Money rhs)
+    {
+        return lhs.Rubles * 100 + lhs.Kopeks == rhs.Rubles * 100 + rhs.Kopeks;
+    }
+
+    public static bool operator !=(Money lhs, Money rhs)
+    {
+        return !(lhs == rhs);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is Money other)
+        {
+            return this == other;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return (Rubles * 100 + Kopeks).GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return $"{Rubles},{Kopeks:D2}";
+    }
 }
