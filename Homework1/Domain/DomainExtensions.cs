5d5102b71ff4fe96bf632bf40b498091a8a9584a
Alekseev Andrey <alekseev.anrdey@nanocad.ru>
2025-03-07 20:14:15 +0300

    extensions imp

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -1,6 +1,23 @@
 ﻿namespace Fuse8.BackendInternship.Domain;
-
 public static class DomainExtensions
 {
-	// ToDo: реализовать экстеншены
+	public static bool IsNullOrEmpty<T>(this IEnumerable<T> value)
+	{
+		return (value == null || !value.Any()) ;
+	}
+
+    public static string JoinToString<T>(this IEnumerable<T> value, string separator)
+	{
+		if (value.IsNullOrEmpty())
+		{
+			throw new ArgumentNullException(nameof(value));
+		}
+
+		return string.Join(separator, value);
+	}
+
+	public static int DaysCountBetweem(this DateTimeOffset StartDate, DateTimeOffset EndDate)
+	{
+		return (EndDate - StartDate).Days;
+	}
 }
