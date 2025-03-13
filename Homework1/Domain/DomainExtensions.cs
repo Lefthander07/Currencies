267af8d31980c0db82bf9308f05e7cfb089c2e68
Alekseev Andrey <alekseev.anrdey@nanocad.ru>
2025-03-13 15:39:53 +0300

    Поправлен DomainExtensions

--- Homework1/Domain/DomainExtensions.cs
+++ Homework1/Domain/DomainExtensions.cs
@@ -1,12 +1,12 @@
 ﻿namespace Fuse8.BackendInternship.Domain;
 public static class DomainExtensions
 {
-	public static bool IsNullOrEmpty<T>(this IEnumerable<T> value)
+	public static bool IsNullOrEmpty<T>(this IEnumerable<T>? value)
 	{
 		return (value == null || !value.Any()) ;
 	}
 
-    public static string JoinToString<T>(this IEnumerable<T> value, string separator)
+    public static string JoinToString<T>(this IEnumerable<T>? value, string separator)
 	{
 		if (value.IsNullOrEmpty())
 		{
@@ -18,6 +18,6 @@ public static class DomainExtensions
 
 	public static int DaysCountBetweem(this DateTimeOffset StartDate, DateTimeOffset EndDate)
 	{
-		return (EndDate - StartDate).Days;
+		return Math.Abs((EndDate - StartDate).Days);
 	}
 }
